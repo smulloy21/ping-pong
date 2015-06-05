b01665fcda8a0e1df004d373270bac62e423ea8c
Shannon Mulloy <smulloy21@gmail.com>
2015-06-04 20:37:58 -0500

    a little faster

--- js/test.js
+++ js/test.js
@@ -43,13 +43,13 @@ for (var i = 1; i <= num; i++) {
 
 $('div p').each(function(i) {
 	if (i % 3 === 0 && i % 5 === 0) {
-		$(this).delay((i++) * 450).fadeTo(200, 1);
+		$(this).delay((i++) * 400).fadeTo(175, 1);
 	} else if (i % 3 === 0) {
-		$(this).delay((i++) * 450).fadeTo(200, 1).animate({right: '30%'}, 200);
+		$(this).delay((i++) * 400).fadeTo(175, 1).animate({right: '30%'}, 175);
 	} else if (i % 5 === 0) {
-		$(this).delay((i++) * 450).fadeTo(200, 1).animate({left: '30%'}, 200);
+		$(this).delay((i++) * 400).fadeTo(175, 1).animate({left: '30%'}, 175);
 	} else {
-		$(this).delay((i++) * 450).fadeTo(200, 1);
+		$(this).delay((i++) * 400).fadeTo(175, 1);
 	}
 });
 
